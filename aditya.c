87554587c3fc0f5f60545cde0047257c5f04d98a
adityaraj7870 <romanraj9256@gmail.com>
2026-02-09 10:34:17 +0530

    Add files via upload

---
+++ aditya.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{ 
+    int x,y,z;
+    printf("Welcome to Btech G2G\n");
+    printf("enter the values of x and y -",x,y);
+    scanf("%d%d",&x,&y);
+    z=x+y;
+    printf("sum is %d",z);
+    return 0;
+
+
+
+
+}
