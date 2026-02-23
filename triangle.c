e35987b12a46068ab78589732843998c5d645faf
adityaraj7870 <romanraj9256@gmail.com>
2026-02-23 11:13:16 +0530

    Add files via upload

---
+++ triangle.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int a,c,d;
+    printf("enter sides of triangle");
+    scanf("%d%d%d",&a,&c,&d);
+    if ((a+c)>d || (c+d)>a || (a+d)>c){
+         printf("yes it can form a triangle\n");
+    }
+    if (a==c==d){
+        printf("it is a equilateral triangle");
+    }
+    else if (a!=d!=c){
+        printf("it is a scalar triangle");
+    }
+    else {
+        printf("it is a isosceles");
+    }
+    return 0;
+}
+
+
